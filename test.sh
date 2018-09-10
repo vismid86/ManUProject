af9c88a41c670ed792b8fa9ef16f7f25dc4b69b8
vismid86 <vismid86@gmail.com>
2018-09-10 10:53:41 +0930

    keytest

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+#`start=SECONDS
+SECONDS=0
 #Test file
 newfile=$(mktemp)
 echo "Created newfile" >> $newfile
 cat $newfile
-
+cat $newfile
+sleep 3
+duration=$SECONDS
+echo $duration
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds"
