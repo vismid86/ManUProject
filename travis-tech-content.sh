f11c0208254ca24dc051a290240d7bae5b1b7941
vismid86 <vismid86@gmail.com>
2019-03-18 14:28:33 +1030

    check travis-temrinate

---
+++ travis-tech-content.sh
@@ -0,0 +1,20 @@
+set -e
+
+CHANGED_FILES=`git diff --name-only HEAD~1`
+IS_CMS=False
+CMS="apps\/contents\/"
+
+for CHANGED_FILE in $CHANGED_FILES; do
+  if [[ $CHANGED_FILE =~ $CMS ]]; then
+    IS_CMS=True
+    break
+  fi
+done
+
+if [[ $IS_CMS == True ]]; then
+  echo "CMS files found, continue"
+else
+  echo "No changes to CMS content; Terminating"
+  travis_terminate 0
+fi
+
